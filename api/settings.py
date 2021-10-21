48f9a5f0698d2bbaefac9803d2bd52b51a258836
Vaishnavi Gupta <vaishnavi_g@me.iitr.ac.in>
2021-10-21 19:05:41 +0530

    added email notification on comment

--- api/settings.py
+++ api/settings.py
@@ -184,3 +184,10 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
+EMAIL_USE_TLS = True
